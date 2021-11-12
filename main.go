66ab8248b0c8dd3e8490ff06c9cd6adf8293b6c4
Jim Raden <james.raden@gmail.com>
2021-11-12 11:42:42 -0500

    Dec  8  2020 - WIP

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+)
 
+func main() {
+	cmd := exec.Command("git", "log", "--oneline", "--first-parent")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmd.Wait()
+	fmt.Printf("%s", &out)
+
+	// log.Printf("Exit code: %v", exitCode)
 }
